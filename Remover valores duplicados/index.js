20825ae95154bcec3330b2cdd8e76312ba9f512e
Maciel-NJ <asbahr.samuel10@outlook.com>
2025-05-26 14:31:00 -0300

    Update index.js

--- Remover valores duplicados/index.js
+++ Remover valores duplicados/index.js
@@ -1,13 +1,15 @@
+//novamente a criação de vetores de valores vazios 
 let vetorOriginal = [];
 let vetorSemDuplicatas = [];
 let tamanho = 0;
 
+// lê os 10 numeros
 for (let i = 0; i < 10; i++) {
     let numero = parseInt(prompt("Digite o " + (i + 1) + "º número:"));
     vetorOriginal[i] = numero;
 }
 
-
+// verifica se o valor original ja está em duplicatas
 for (let i = 0; i < 10; i++) {
     let existe = false;
     for (let j = 0; j < tamanho; j++) {
@@ -16,11 +18,13 @@ for (let i = 0; i < 10; i++) {
         }
     }
 
+    //se não tiver ele adiciona 1 
     if (existe === false) {
         vetorSemDuplicatas[tamanho] = vetorOriginal[i];
         tamanho = tamanho + 1;
     }
 }
 
+//mostra o vetor original e depois sem duplicatas
 console.log("Vetor original:", vetorOriginal);
 console.log("Vetor sem duplicatas:", vetorSemDuplicatas);
