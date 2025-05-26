7e8998699e997fcb4ea2b8ab7ea940650d53329a
Maciel-NJ <asbahr.samuel10@outlook.com>
2025-05-26 14:23:08 -0300

    Update index.js

--- Vetor com 7 números simulados como aleatórios/index.js
+++ Vetor com 7 números simulados como aleatórios/index.js
@@ -1,6 +1,8 @@
+//vetor vazio para armazenar números que ainda virão
 let vetor = [];
 let base = 17; 
 
+//gera números aleatórios, mas somente de 0 até 100
 for (let i = 0; i < 7; i++) {
     base = (base * 3 + 7) % 101; 
     if (base === 0) {
@@ -9,6 +11,7 @@ for (let i = 0; i < 7; i++) {
     vetor[i] = base;
 }
 
+//irá mostrar os valores gerados
 console.log("Números gerados:");
 for (let i = 0; i < 7; i++) {
     console.log(vetor[i]);
